4d637854100c0fc38a6fa41dcc144d40f3135eda
norouzy <mohamadnewday@gmail.com>
2023-05-30 15:29:16 +0330

    disable btn

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -6,6 +6,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -43,7 +49,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className="error-text">Name is empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
